51317ddc0747801433fdb6a24fc481e743d8df5b
Graham Myhre <graham@opinyun.com>
2013-05-26 16:44:10 -0700

    test

--- config/initializers/ssl_dev.rb
+++ config/initializers/ssl_dev.rb
@@ -1,4 +1,4 @@
 # http://stackoverflow.com/questions/3977303/omniauth-facebook-certificate-verify-failed/5618072#5618072
 #if Rails.env.development? 
-#  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 #end
